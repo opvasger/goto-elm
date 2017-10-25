b4e3a291b5fba50fd4e4a484f2fadc79be39f839
opvasger <an@zispa.dk>
2017-10-25 15:45:39 +0200

    Counter example

---
+++ source/Counter.elm
@@ -0,0 +1,39 @@
+module Counter exposing (main)
+
+import Html
+import Html.Events as Events
+
+
+type Message
+    = Increment
+    | Decrement
+
+
+update message model =
+    case message of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+model =
+    0
+
+
+view model =
+    Html.div
+        []
+        [ Html.button [ Events.onClick Increment ] [ Html.text "+" ]
+        , Html.div [] [ Html.text (toString model) ]
+        , Html.button [ Events.onClick Decrement ] [ Html.text "-" ]
+        ]
+
+
+main =
+    Html.beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
